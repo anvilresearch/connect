da47850d97db43ce21cc7982c6c0d620baf88e56
Christian Smith <smith@anvil.io>
2014-07-30 17:54:00 -0700

    replace verifyUserToken middleware with verifyAccessToken; deprecate verifyUserToken

--- routes/userinfo.js
+++ routes/userinfo.js
@@ -12,9 +12,13 @@ module.exports = function (server) {
   server.get('/userinfo',
     oidc.parseAuthorizationHeader,
     oidc.getBearerToken,
-    oidc.verifyUserToken(server, 'profile'),
+    oidc.verifyAccessToken({
+      iss:    server.settings.issuer,
+      key:    server.settings.publicKey,
+      scope: 'profile'
+    }),
     function (req, res, next) {
-      User.get(req.token.sub, function (err, user) {
+      User.get(req.claims.sub, function (err, user) {
         if (err)   { return next(err); }
         if (!user) { return next(new NotFoundError()); }
         res.json(200, user.project('userinfo'));
