bd316cf0a68370978a4c658b1b6dab2a4f099da0
Christian Smith <smith@anvil.io>
2015-03-05 10:12:57 -0800

    load provider modules by iterating through directory

--- providers/index.js
+++ providers/index.js
@@ -2,55 +2,28 @@
  * Module dependencies
  */
 
-var settings = require('../boot/settings');
+var fs = require('fs')
+  , path = require('path')
+  , settings = require('../boot/settings')
+  ;
 
 
 /**
- * Provider definitions
+ * Read providers directory
  */
 
-module.exports = {
-
-  /**
-   * Password
-   */
-
-  'password':   require('./password')(settings),
+var files = fs.readdirSync(path.join('.', 'providers'));
 
 
-  /**
-   * OAuth 1.0 Providers
-   */
+/**
+ * Load providers
+ */
 
-  'twitter':    require('./twitter')(settings),
-
-
-  /**
-   * OAuth 2.0 Providers
-   */
-
-  '37signals':  require('./37signals')(settings),
-  'angellist':  require('./angellist')(settings),
-  'buffer':     require('./buffer')(settings),
-  'dropbox':    require('./dropbox')(settings),
-  'facebook':   require('./facebook')(settings),
-  'foursquare': require('./foursquare')(settings),
-  'github':     require('./github')(settings),
-  'google':     require('./google')(settings),
-  'instagram':  require('./instagram')(settings),
-  'linkedin':   require('./linkedin')(settings),
-  'mailchimp':  require('./mailchimp')(settings),
-  'reddit':     require('./reddit')(settings),
-  'soundcloud': require('./soundcloud')(settings),
-  'twitch':     require('./twitch')(settings),
-  'wordpress':  require('./wordpress')(settings),
-
-
-  /**
-   * Test Providers
-   */
-
-  'oauthtest':  require('./oauthtest')(settings),
-  'oauth2test': require('./oauth2test')(settings),
-
-};
+files.forEach(function (file) {
+  if (path.extname(file) === '.js' && file !== 'index.js') {
+    var provider = path.basename(file, '.js');
+    module.exports[provider] = require(
+      path.join('..', 'providers', provider)
+    )(settings);
+  }
+});
