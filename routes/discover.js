6b8077c8f39c88b5e3939248f098bd1cf0b3bddc
Christian Smith <smith@anvil.io>
2015-06-09 12:38:34 -0700

    fix(discovery): include all registered scopes in server metadata

--- routes/discover.js
+++ routes/discover.js
@@ -1,4 +1,11 @@
 /**
+ * Module dependencies
+ */
+
+var Scope = require('../models/Scope');
+
+
+/**
  * Well-Known Endpoint
  */
 
@@ -9,7 +16,18 @@ module.exports = function (server) {
    */
 
   server.get('/.well-known/openid-configuration', function (req, res, next) {
-    res.json(server.OpenIDConfiguration);
+    Scope.list(function (err, scopes) {
+      if (err) { return next(err); }
+
+      // Get a list of scope names
+      scopes = scopes.map(function (scope) {
+        return scope.name;
+      });
+
+      // Update the OpenIDConfiguration and respond
+      server.OpenIDConfiguration.scopes_supported = scopes;
+      res.json(server.OpenIDConfiguration);
+    });
   });
 
 };
