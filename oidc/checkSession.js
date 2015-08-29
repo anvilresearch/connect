a90168595fd00e241e4e770b81f49c1b9c3ddcba
Christian Smith <smith@anvil.io>
2015-08-29 15:00:21 -0700

    fix(checkSession): only issue event once per opbs change

--- oidc/checkSession.js
+++ oidc/checkSession.js
@@ -10,22 +10,26 @@ var client = require('../boot/redis').getClient()
 
 function checkSession (req, res, next) {
   var initialOpbs = req.session.opbs
-  client.get('sess:' + req.sessionID, function (err, data) {
-    if (err) { return next(err) }
-    if (data) {
-      try {
-        var opbs = JSON.parse(data).opbs
-        if (opbs !== initialOpbs) {
-          res.write('event: update\n')
-          res.write('data: ' + opbs + '\n\n')
-        }
-      } catch (e) {}
-    }
 
-    setTimeout(function () {
-      checkSession(req, res)
-    }, 3000)
-  })
+  function compareOpbs () {
+    client.get('sess:' + req.sessionID, function (err, data) {
+      if (err) { return next(err) }
+      if (data) {
+        try {
+          var opbs = JSON.parse(data).opbs
+          if (opbs !== initialOpbs) {
+            res.write('event: update\n')
+            res.write('data: ' + opbs + '\n\n')
+            initialOpbs = opbs
+          }
+        } catch (e) {}
+      }
+
+      setTimeout(function () {
+        compareOpbs()
+      }, 3000)
+    })
+  }
 }
 
 /**
