8a5f8e7656bc7516013e1c7b2a87f034ca9df6bf
Vartan Simonian <vsimonianpress@gmail.com>
2015-09-05 17:42:46 -0700

    fix(oidc/error): Improve error-handling and display
    In development environment, will also output more details on 401, 403 and 500 errors

--- oidc/error.js
+++ oidc/error.js
@@ -9,10 +9,16 @@ var qs = require('qs')
  */
 
 function error (err, req, res, next) {
+  var message
+  var errorName = err.error || err.message || err.name
+  var errorDescription = err.error_description || err.description || err.message
   // 302 Redirect
   if (err.statusCode === 302 && err.redirect_uri) {
-    var responseMode = '?' || '#'
-    var error = { error: err.error, error_description: err.error_description }
+    var params = req.connectParams || req.query
+    var responseMode = (params.response_mode && params.response_mode.trim()) ||
+      (params.response_type === 'code' ||
+        params.response_type === 'none') ? '?' : '#'
+    var error = { error: errorName, error_description: errorDescription }
     var uri = err.redirect_uri + responseMode + qs.stringify(error)
 
     res.redirect(uri)
@@ -25,8 +31,8 @@ function error (err, req, res, next) {
     })
 
     res.status(400).json({
-      error: err.error || err.message,
-      error_description: err.error_description,
+      error: errorName,
+      error_description: errorDescription,
       error_uri: err.error_uri
     })
 
@@ -37,20 +43,30 @@ function error (err, req, res, next) {
     res.set({
       'WWW-Authenticate': 'Bearer ' +
         'realm="' + err.realm + '", ' +
-        'error="' + err.error + '", ' +
-        'error_description="' + err.error_description + '"'
+        'error="' + errorName + '", ' +
+        'error_description="' + errorDescription + '"'
     })
 
-    res.status(401).send('Unauthorized')
+    res.status(401).send('Unauthorized<br><br>' + errorDescription)
 
   // 403 Forbidden
   } else if (err.statusCode === 403) {
-    res.status(403).send('Forbidden')
+    message = 'Forbidden'
+
+    if (req.app.get('env') === 'development') {
+      message += '<br><br>' + errorDescription
+    }
+
+    res.status(403).send(message)
 
   // 500 Internal Server Error
   } else {
     var statusCode = err.statusCode || 500
-    var message = (err.statusCode && err.message) || 'Internal Server Error'
+    message = (err.statusCode && err.message) || 'Internal Server Error'
+
+    if (req.app.get('env') === 'development') {
+      message += '<br><br>' + errorDescription
+    }
 
     res.status(statusCode).send(message)
   }
