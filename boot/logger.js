c98416932e8a53cfdd5d8154332e30f65917a429
Joris Kraak <me@joriskraak.nl>
2015-12-22 10:55:45 +0100

    fix(logger): ensure writable `logs` folder on boot

--- boot/logger.js
+++ boot/logger.js
@@ -4,6 +4,7 @@
 
 var cwd = process.cwd()
 var env = process.env.NODE_ENV || 'development'
+var fs = require('fs')
 var path = require('path')
 var bunyan = require('express-bunyan-logger')
 
@@ -14,8 +15,31 @@ var bunyan = require('express-bunyan-logger')
 var streams = []
 
 if (!env.match(/test/i)) {
+  var logs_path = path.join(cwd, 'logs')
+
+  fs.stat(logs_path, function (err, logs_path_stats) {
+    if (err && err.code === 'ENOENT') {
+      fs.mkdir(logs_path, function (err) {
+        if (err) {
+          console.error('Could not create the logs path [%s]', logs_path)
+          process.exit(1)
+        }
+      })
+    } else if (!logs_path_stats.isDirectory()) {
+      console.error('The logs path [%s] is not a directory', logs_path)
+      process.exit(1)
+    }
+
+    fs.access(logs_path, fs.W_OK, function (err) {
+      if (err && err.code === 'EACCES') {
+        console.error('The logs path [%s] is not writable', logs_path)
+        process.exit(1)
+      }
+    })
+  })
+
   streams.push({ stream: process.stdout })
-  streams.push({ path: path.join(cwd, 'logs', env + '.log') })
+  streams.push({ path: path.join(logs_path, env + '.log') })
 }
 
 /**
