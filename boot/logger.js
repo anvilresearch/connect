45227e973a80cda00c3ee00ab65d9b94998e498d
Jonathan Lifschutz <jonathan.lifschutz@cantab.net>
2016-03-02 23:01:22 +0000

    feat(logger): added debug and error logging to oidc, configurable in config file

--- boot/logger.js
+++ boot/logger.js
@@ -7,6 +7,45 @@ var env = process.env.NODE_ENV || 'development'
 var path = require('path')
 var bunyan = require('express-bunyan-logger')
 var ensureWritableDirectory = require('../lib/fs-utils').ensureWritableDirectory
+var oidc = require('../oidc')
+
+var FN_ARGS_SPLIT = /^[^\(]*\(\s*([^\)]*)\)/m
+
+function getSignature (fn) {
+  return fn.toString().match(FN_ARGS_SPLIT)[1]
+}
+
+function oidcLogging (addLogging) {
+  for (var idx in oidc) {
+    var fn = oidc[idx]
+    if (getSignature(fn) === addLogging.signature) {
+      // wrap function with logging
+      oidc[fn.name] = addLogging(fn)
+    }
+  }
+}
+
+function debugLogging () {
+  var addLogging = function (fn) {
+    return function (req, res, next) {
+      req.log.debug(fn.name)
+      fn(req, res, next)
+    }
+  }
+  addLogging.signature = 'req, res, next'
+  oidcLogging(addLogging)
+}
+
+function errorLogging () {
+  var addLogging = function (fn) {
+    return function (err, req, res, next) {
+      req.log.error(err, fn.name)
+      fn(err, req, res, next)
+    }
+  }
+  addLogging.signature = 'err, req, res, next'
+  oidcLogging(addLogging)
+}
 
 /**
  * Export
@@ -14,7 +53,7 @@ var ensureWritableDirectory = require('../lib/fs-utils').ensureWritableDirectory
 
 module.exports = function (options) {
   var logger
-  var config = { name: 'request', streams: [] }
+  var config = { name: 'request', streams: [], level: 'error' }
 
   if (!env.match(/test/i)) {
     var logsPath = path.join(cwd, 'logs')
@@ -22,6 +61,29 @@ module.exports = function (options) {
 
     if (options && options.stdout) { config.streams.push({ stream: process.stdout }) }
     if (options && options.file) { config.streams.push({ path: path.join(logsPath, env + '.log') }) }
+    if (options && options.level) { config.level = options.level }
+
+    switch (config.level) {
+      case 'fatal':
+        break
+      case 'error':
+        errorLogging()
+        break
+      case 'warn':
+        errorLogging()
+        break
+      case 'info':
+        errorLogging()
+        break
+      case 'debug':
+        debugLogging()
+        errorLogging()
+        break
+      case 'trace':
+        debugLogging()
+        errorLogging()
+        break
+    }
 
     try {
       config = require('../logger')
@@ -31,6 +93,7 @@ module.exports = function (options) {
   }
 
   logger = bunyan(config)
+
   module.exports = logger
   return logger
 }
