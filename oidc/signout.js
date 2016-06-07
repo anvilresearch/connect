3c4084c2309d71422ceaff05c9995c7a799e5b26
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-06-07 13:11:07 -0400

    Fix POST handling by /signout endpoint
    Fixes #336.

--- oidc/signout.js
+++ oidc/signout.js
@@ -1,67 +1,73 @@
 /**
  * Module dependencies
  */
-
 var authenticator = require('../lib/authenticator')
 var settings = require('../boot/settings')
 var Client = require('../models/Client')
 var IDToken = require('../models/IDToken')
 
 /**
- * Signout
+ * Handles signout requests (GETs or POSTs to the /signout endpoint) and ends a
+ * user's session. If an ID Token hint and a post-signout URI are provided,
+ * verifies that the URI has been pre-registered, signs out, and redirects the
+ * user to it. Otherwise, just signs out with no redirect.
+ * @method signout
+ * @param [req.query.id_token_hint] {String}
+ * @param [req.body.id_token_hint] {String} Id Token hint, identifies the
+ *   client/user
+ * @param [req.query.post_logout_redirect_uri] {String}
+ * @param [req.body.post_logout_redirect_uri] {String} If specified, this
+ *   handler verifies that the uri was pre-registered, and sends a redirect to
+ *   it via an HTTP 303 See Other response.
+ * @param [req.query.state] {String}
+ * @param [req.body.state] {String} Opaque OIDC state parameter
  */
-
 function signout (req, res, next) {
-  var uri = req.query.post_logout_redirect_uri
-  var hint = req.query.id_token_hint
-  var token = uri && hint && IDToken.decode(hint, settings.keys.sig.pub)
+  var idToken, clientId
+  var params = req.query || req.body
+  var postLogoutUri = params.post_logout_redirect_uri
+  var idHint = params.id_token_hint
+  var state = params.state
 
-  // verify the uri using the hint
-  if (token && token instanceof Error === false) {
-    Client.get(token.payload.aud, function (err, client) {
-      var uris = client && client.post_logout_redirect_uris
-
-      // something smells bad
-      if (err) {
-        return next(err)
-
-      // unknown client, or the uri is not registered.
-      // logout, but don't redirect.
-      } else if (!uris || uris.indexOf(uri) === -1) {
-        authenticator.logout(req)
-
-        res.set({
-          'Cache-Control': 'no-store',
-          'Pragma': 'no-cache'
-        })
-
-        res.sendStatus(204)
-
-      // logout and redirect
-      } else {
-        authenticator.logout(req)
-        if (req.query.state) {
-          uri += '?state=' + req.query.state
-        }
-        res.redirect(uri)
-      }
-    })
-
-  // logout and respond without redirect
-  } else {
-    authenticator.logout(req)
-
-    res.set({
-      'Cache-Control': 'no-store',
-      'Pragma': 'no-cache'
-    })
-
-    res.sendStatus(204)
+  if (idHint) {
+    idToken = IDToken.decode(idHint, settings.keys.sig.pub)
+    if (idToken instanceof Error) { return next(idToken) }
+    clientId = idToken.payload.aud
   }
+  if (idToken && postLogoutUri) {
+    // Verify the post-signout uri (must have been registered for this client)
+    Client.get(clientId, function (err, client) {
+      if (err) { return next(err) }
+      var isValidUri = false
+      if (client) {
+        var registeredUris = client.post_logout_redirect_uris
+        isValidUri = registeredUris &&
+          (registeredUris.indexOf(postLogoutUri) !== -1)
+      }
+      if (isValidUri) {
+        if (state) {
+          postLogoutUri += '?state=' + state
+        }
+        // sign out and redirect
+        authenticator.logout(req)
+        res.redirect(303, postLogoutUri)
+        return
+      }
+      // Otherwise, fall through to default case below
+    })
+  }
+  // Handle all the other cases - no postLogoutUri specified, or the client is
+  // unknown, or the given postLogoutUri has not been registered previously.
+  // Do not redirect, simply sign out
+  authenticator.logout(req)
+  res.set({
+    'Cache-Control': 'no-store',
+    'Pragma': 'no-cache'
+  })
+  res.sendStatus(204)
 }
 
 /**
  * Export
  */
-
 module.exports = signout
